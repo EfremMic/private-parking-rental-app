11ca17787f87030c27c28f439dbd7ad3d523bd44
EfremMic <urbanlamda@gmail.com>
2024-12-17 17:07:11 +0100

    Deleted all files from main branch

--- notification-service/src/main/java/com/parking/notificationservice/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.parking.notificationservice.repository;
-
-import com.parking.notificationservice.model.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
--- parking-service/src/main/java/com/parking/spotlisting/parkingservice/repository/ParkingSpotRepository.java
+++
@@ -1,7 +0,0 @@
-package com.parking.spotlisting.parkingservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.parking.spotlisting.parkingservice.model.ParkingSpot;
-
-public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Long> {
-}
